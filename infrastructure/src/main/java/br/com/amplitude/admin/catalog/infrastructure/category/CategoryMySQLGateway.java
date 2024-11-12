d49346201d7842647578e9143c1f8397cc2d0e17
João Antônio Siqueira <joao.calvet.siqueira@gmail.com>
2024-11-12 17:20:00 -0300

    implementing the create category feature

--- infrastructure/src/main/java/br/com/amplitude/admin/catalog/infrastructure/category/CategoryMySQLGateway.java
+++ infrastructure/src/main/java/br/com/amplitude/admin/catalog/infrastructure/category/CategoryMySQLGateway.java
@@ -5,6 +5,7 @@ import br.com.amplitude.admin.catalog.domain.category.CategoryGateway;
 import br.com.amplitude.admin.catalog.domain.category.CategoryID;
 import br.com.amplitude.admin.catalog.domain.category.CategorySearchQuery;
 import br.com.amplitude.admin.catalog.domain.pagination.Pagination;
+import br.com.amplitude.admin.catalog.infrastructure.category.persistence.CategoryJpaEntity;
 import br.com.amplitude.admin.catalog.infrastructure.category.persistence.CategoryRepository;
 import org.springframework.stereotype.Service;
 
@@ -21,7 +22,7 @@ public class CategoryMySQLGateway implements CategoryGateway {
 
     @Override
     public Category create(Category aCategory) {
-        return null;
+        return repository.save(CategoryJpaEntity.from(aCategory)).toAggregate();
     }
 
     @Override
